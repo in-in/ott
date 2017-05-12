5e59a3600f85d87c8321b83b54646ec1967df2d8
in-in <0866222@gmail.com>
2017-05-12 21:54:10 +0300

    init commit

---
+++ .config/postcss.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: [
+    require('postcss-easy-import')(),
+    require('postcss-nested-ancestors'),
+    require('postcss-nested')({
+      bubble: ['each']
+    }),
+    require('postcss-custom-properties').default(),
+    require('postcss-custom-media')(),
+    require('postcss-media-minmax')(),
+    require('postcss-each')(),
+    require('postcss-assets')({
+      'basePath': 'src/'
+    }),
+    require('postcss-color-function')(),
+    require('postcss-pseudoelements')(),
+    require('autoprefixer')({
+      'browsers': 'last 10 versions'
+    }),
+    require('css-mqpacker')({
+      sort: true
+    }),
+    require('postcss-csso')()
+  ]
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Newla
+
+A tiny `npm-scripts-driven` project template for small tasks in HTML and CSS.
+
+### Get your copy
+
+```bash
+git clone git@github.com:in-in/newla.git project && cd project
+```
+
+### Create a new empty Git and install the packages
+
+```bash
+npm run init-start
+```
+
+### Run and watch
+
+```bash
+npm start
+```
+
+### Easy update all dependencies
+
+```bash
+npm run ncu
+```
+
+If you need some sample code, look at the `example` branch
