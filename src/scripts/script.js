76452f8c6e48779684cb0bc17e8f6e60ea2a5bda
in-in <0866222@gmail.com>
2017-05-14 10:08:50 +0300

    add menu

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,9 +5,24 @@ $( document ).ready(function() {
     
     $menuToggle.click(function(e) {
       e.preventDefault();
+      $(this).toggleClass('menu__toggle--open');
       $menuList.slideToggle(200, function() {
         if($(this).css('display') === 'none') {
           $(this).removeAttr('style');
+        }
+      });
+    });
+    //submenu
+    var $menuLinkMore = $('.menu__link-more');
+    var $menuSubmenu = $('.menu__submenu');
+    
+    $menuLinkMore.click(function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $menuSubmenu.toggleClass('menu__submenu--open');
+      $(window).on('click keydown', function(e) {
+        if (e.type === 'click' || e.keyCode === 27 && $menuSubmenu.hasClass('menu__submenu--open')) {
+          $menuSubmenu.removeClass('menu__submenu--open');
         }
       });
     });
